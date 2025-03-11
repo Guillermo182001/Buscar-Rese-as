96dd8b59901648202c1caff8275d181c65e4857d
Guillermo <gmiesesaliaga@gmail.com>
2025-03-10 22:17:32 -0500

    fix(dev-server): configurar ngrok y corregir 'Invalid Host Header'
    Se configuró el devServer en vue.config.js para aceptar conexiones externas.
    También se corrigieron errores de WebSocket.

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 8080,
+    allowedHosts: "all",
+    host: "0.0.0.0", // Acepta conexiones externas (necesario para ngrok)
+    client: {
+      webSocketURL: 'wss://3f5b-38-25-17-101.ngrok-free.app/ws',
+    }
+  }
 })
