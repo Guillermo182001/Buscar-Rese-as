925a937ccab27115f7aa23f7198caaa5cec44ad7
Guillermo <gmiesesaliaga@gmail.com>
2025-03-12 20:31:41 -0500

    feat(rutas): aplicar limitación de tasa a rutas de API

--- api/routes/api.php
+++ api/routes/api.php
@@ -3,11 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReviewController;
 use App\Http\Middleware\CorsMiddleware;
+use App\Http\Middleware\ApiRateLimiter;
 
 Route::get('/test', function () {
     return response()->json(['message' => 'Conexión exitosa!']);
 });
-Route::middleware([CorsMiddleware::class])->group(function () {
+
+// Aplicar middleware de CORS y Rate Limiting a todas las rutas de la API
+Route::middleware([
+    \App\Http\Middleware\CorsMiddleware::class,
+    \App\Http\Middleware\ApiRateLimiter::class
+])->group(function () {
     Route::get('/reviews/google', [ReviewController::class, 'getGoogleReviews']);
     Route::get('/reviews/facebook', [ReviewController::class, 'getFacebookReviews']);
     Route::get('/test-apis', [ReviewController::class, 'testApis']);
